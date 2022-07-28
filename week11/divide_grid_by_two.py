40b48d4941165768763ea052dd84f20e08478bbc
seunghwan Choi <dexrf1@gmail.com>
2022-07-28 20:01:33 +0900

    add: 전력망 둘로 나누기

--- week11/divide_grid_by_two.py
+++ week11/divide_grid_by_two.py
@@ -20,11 +20,11 @@ def solution(n, wires):
     # root는 1번노드로 가정하고, DFS로 각 노드의 자손 노드의 개수를 찾는다
     number_of_descendant[1] = find_the_number_of_descendant(1, adjacent_list, number_of_descendant, visited)
 
-    # 자손 노드의 개수에 1을 더하여 각 노드의 크기를 계산한다.
-    # 노드의 크기 = 자기 자신을 root로 하는 서브트리의 모든 노드의 개수
-    subtree = list(map(lambda x: x + 1, number_of_descendant[2:]))
-    absolute = list(map(lambda x: abs((n - 2 * x)), subtree))
-    answer = min(absolute)
+    # 자손 노드의 개수에 1을 더하여 각 서브트리의 크기를 구한다.
+    # 서브트리의 크기 = 자기 자신을 root로 하는 서브트리의 모든 노드의 개수
+    size_of_subtrees = list(map(lambda x: x + 1, number_of_descendant[2:]))
+    diff = list(map(lambda x: abs((n - 2 * x)), size_of_subtrees))
+    answer = min(diff)
 
     return answer
 
